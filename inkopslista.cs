f4954615bc83481dc4a7230ef88d1ddb90d36865
RamzoCodes <ramizkhurshid@gmail.com>
2026-09-10 14:06:18 +0200

    Skapade en bool som ska fortsätta tills den blir true, sedan kommer text som frågar om vara, då ska man svara den med varans namn, sedan kommer en till text som frågar om priset, då ska man lägga in priset som är en heltal. Sedan läggs dessa inmatningar in i listorna.

--- inkopslista.cs
+++ inkopslista.cs
@@ -3,4 +3,20 @@
 
 List<string> Varor = []; // Varorna tillhör string
 List<int> pris = []; // priserna tillhör int
- 
+
+Console.WriteLine("Hej och välkommen till inköpslistan!");
+
+bool klar = false; 
+
+while (!klar)
+{
+    Console.WriteLine("Ange vara");
+    string Inputvara = Console.ReadLine();
+
+    Console.WriteLine("Ange priset");
+    int Inputpris = int.Parse(Console.ReadLine());
+
+    Varor.Add(Inputvara);
+    pris.Add(Inputpris);
+    
+}
