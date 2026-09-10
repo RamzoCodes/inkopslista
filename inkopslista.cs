43ee27a9d6887cbd112d3fad497233e37af8b611
RamzoCodes <ramizkhurshid@gmail.com>
2026-09-10 13:19:45 +0200

    Har skapat och angett syftet till uppgiften

---
+++ inkopslista.cs
@@ -0,0 +1,6 @@
+// Syftet med denna uppgiften är att göra en lista som innehåller varor och dess pris
+
+
+List<string> Varor = []; // Varorna tillhör string
+List<int> pris = []; // priserna tillhör int
+ 
